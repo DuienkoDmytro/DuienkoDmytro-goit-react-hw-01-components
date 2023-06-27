79f6ea07f2cee55f09e90da73e52d315900ddf82
DuienkoDmytro <119892965+DuienkoDmytro@users.noreply.github.com>
2023-06-27 14:08:40 +0300

    update_files

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+import FriendListItem from './FriendListItem';
+import css from './friendlist.module.css';
+
+export default function FriendList({ friends }) {
+    return <ul className={css.friendlist}>
+        {friends.map(friend => (<FriendListItem name={friend.name} key={friend.id} isOnline={friend.isOnline} avatar={friend.avatar} />))}
+    </ul>
+}
+
+// FriendList.propTypes = {
+//    friends: PropTypes.shape({
+// name: PropTypes.string,
+// id: PropTypes.number,
+// avatar: PropTypes.string,
+// isOnline: PropTypes.bool,
+// }) 
+// };
+
+friends: PropTypes.arrayOf(
+  PropTypes.shape({
+    name: PropTypes.string,
+id: PropTypes.number,
+avatar: PropTypes.string,
+isOnline: PropTypes.bool,
+  })
+)
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import css from "./transactionhistory.module.css"; 
+
+export default function TransactionHistory({ items }) {
+    return <table className={css.transactionhistory}>
+        <thead>
+            <tr>
+                <th>Type</th>
+                <th>Amount</th>
+                <th>Currency</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            {items.map(item => (<tr key = {item.id}>
+                <td>{item.type}</td>
+                <td>{item.amount}</td>
+                <td>{item.currency}</td>
+            </tr>))}
+        </tbody>
+    </table>
+}
+
+TransactionHistory.propTypes = {
+   transactions: PropTypes.shape({
+id: PropTypes.number,
+type: PropTypes.string,
+amount: PropTypes.number,
+currency: PropTypes.string,
+}) 
+};
